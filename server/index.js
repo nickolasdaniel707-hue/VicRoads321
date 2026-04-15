eb0a7357525a79f4648d390d3441db42e8740146
nickolasdaniel707-hue <nickolasdaniel707@gmail.com>
2026-04-16 02:31:39 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error(err));
 
 const User = mongoose.model("User", {
   profilePhoto: String,
@@ -52,4 +54,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
