5bf5c0ea4a50c998e8799a9973e3fcdb56faa5c0
nickolasdaniel707-hue <nickolasdaniel707@gmail.com>
2026-04-16 01:46:24 +1000

    Update App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
 
     const res = await fetch("/upload", {
       method: "POST",
-      body: fd
+      body: fd,
     });
 
     const data = await res.json();
@@ -17,9 +17,9 @@ export default function App() {
   };
 
   return (
-    <div style={{padding:20}}>
+    <div style={{ padding: 20 }}>
       <h1>Upload Image</h1>
-      <input type="file" onChange={e => upload(e.target.files[0])} />
+      <input type="file" onChange={(e) => upload(e.target.files[0])} />
       {img && <img src={img} width="200" />}
     </div>
   );
