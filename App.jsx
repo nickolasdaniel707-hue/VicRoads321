c9643e6b111845af7c959ebdc6297b5c46996233
nickolasdaniel707-hue <nickolasdaniel707@gmail.com>
2026-04-16 05:04:08 +0000

    fix react error

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>VicRoads App Working ✅</h1>
+      <p>If you see this, React is fixed.</p>
+    </div>
+  );
+}
+
+export default App;
